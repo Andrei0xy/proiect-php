695155260886ea0e67047acefbb7cae08ef681f9
Andrei0xy <andrei_moisa09@yahoo.com>
2025-01-07 21:50:11 +0200

    Add files via upload

--- casa_productie_muzica/config/routes.php
+++ casa_productie_muzica/config/routes.php
@@ -5,14 +5,34 @@ $routes = [
     "casa_productie_muzica/users/create" => ["UserController", "create"],
     "casa_productie_muzica/users/show" => ["UserController", "show"],
     "casa_productie_muzica/users/delete" => ["UserController", "delete"],
+    "casa_productie_muzica/users/export_excel" => ["UserController", "excel_export"],
+    "casa_productie_muzica/users/export_word" => ["UserController", "word_export"],
+    "casa_productie_muzica/users/export_pdf" => ["UserController", "pdf_export"],
 
     "casa_productie_muzica/artists/index" => ["ArtistController", "index"],
     "casa_productie_muzica/artists/show" => ["ArtistController", "show"],
     "casa_productie_muzica/artists/edit" => ["ArtistController", "edit"],
     "casa_productie_muzica/artists/create" => ["ArtistController", "create"],
     "casa_productie_muzica/artists/delete" => ["ArtistController", "delete"],
+
+    "casa_productie_muzica/albums/index" => ["AlbumController", "index"],
+    "casa_productie_muzica/albums/show" => ["AlbumController", "show"],
+    "casa_productie_muzica/albums/edit" => ["AlbumController", "edit"],
+    "casa_productie_muzica/albums/create" => ["AlbumController", "create"],
+    "casa_productie_muzica/albums/showfor" => ["ArtistController", "showfor"],
+    "casa_productie_muzica/albums/delete" => ["AlbumController", "delete"],
+
+    "casa_productie_muzica/songs/index" => ["AlbumController", "showsongs"],
+    "casa_productie_muzica/songs/edit" => ["SongController", "edit"],
+    "casa_productie_muzica/songs/delete" => ["SongController", "delete"],
+    "casa_productie_muzica/songs/create" => ["SongController", "create"],
+
     "casa_productie_muzica/authentification/login" => ["loginController", "log"],
+    "casa_productie_muzica/authentification/signup" => ["loginController", "signup"],
     "casa_productie_muzica/authentification/logout" => ["loginController", "logout"],
+    "casa_productie_muzica/authentification/signupsuccess" => ["loginController", "success"],
+
+    "casa_productie_muzica/contact/email" => ["ContactController", "email"],
     "casa_productie_muzica" => ["loginController", "home_page"]
 ];
 
@@ -26,6 +46,7 @@ class Router {
 
     public function direct() {
         global $routes;
+        //echo $_GET['id'];
         //echo $this->uri;
         //echo $routes[$this->uri];
         //unset($_SESSION['user_id']);
